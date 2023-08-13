ad2662961a167b96226dcb679cde81cddce0a76b
Charbel Sarkis <charbelsako@gmail.com>
2023-08-13 20:18:59 +0300

    T-16 fixing route bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './components/Login';
 import Protected from './components/Protected';
@@ -10,15 +8,16 @@ import RequireAuth from './components/RequireAuth';
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='login' element={<Login />} />
-          <Route element={<RequireAuth />}>
-            <Route path='protected' element={<Protected />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='login' element={<Login />} />
+            <Route element={<RequireAuth />}>
+              <Route path='protected' element={<Protected />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-      <img src={logo} className='App-logo' alt='logo' />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
