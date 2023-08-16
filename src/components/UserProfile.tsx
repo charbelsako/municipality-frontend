81c61b0846c32afa77c0ab8e6a1aa23797501de8
Charbel Sarkis <charbelsako@gmail.com>
2023-08-16 20:37:50 +0300

    T-23 adds profile component

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,43 @@
+import { useContext, useState } from 'react';
+import AuthContext from '../context/AuthProvider';
+
+export interface IUser {
+  password: string;
+  name: IName;
+  phoneNumberList: [string];
+  email: string;
+  role: string[];
+  refreshToken?: string;
+  personalInfo: IPersonalInfo;
+  sex: string;
+  recordInfo: IRecordInfo;
+  dateOfBirth: Date;
+}
+
+export interface IName {
+  firstName: string;
+  fatherName?: string;
+  motherName?: string;
+  lastName: string;
+}
+
+export interface IPersonalInfo {
+  sect: string;
+}
+
+export interface IRecordInfo {
+  sect: string;
+  number: number;
+}
+
+function UserProfile() {
+  // const [details, setDetails] = useState<IUser>();
+  const { auth, setAuth } = useContext(AuthContext);
+  // useEffect(() => {
+  //   getUserDetails
+  // }, [])
+
+  return <div>{auth.email}</div>;
+}
+
+export default UserProfile;
