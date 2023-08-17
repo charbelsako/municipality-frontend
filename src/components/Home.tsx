b31fe9ff6a2c388df07a6fa6c080ed8b29f04ea7
Charbel Sarkis <charbelsako@gmail.com>
2023-08-17 20:35:56 +0300

    T-24 adds navbar component
    adds useLogout hook
    
    properly sets authentication details

---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Home</h1>
+      <br />
+      <p>You are logged in!</p>
+      <br />
+      <Link to='/create-statement'>Go to create statement page</Link>
+      {/* <br />
+      <Link to='/admin'>Go to the Admin page</Link>
+      <br />
+      <Link to='/lounge'>Go to the Lounge</Link>
+      <br />
+      <Link to='/linkpage'>Go to the link page</Link> */}
+    </section>
+  );
+};
+
+export default Home;
