7a3c8ecdbec5901f58a3c7659b2dc46554343fdf
Charbel Sarkis <charbelsako@gmail.com>
2023-08-31 16:42:43 +0300

    T-37 adds templates for forgot / reset password

---
+++ src/components/ResetPassword.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// this route is the link that will be sent to the user's email address with token and user id
+
+const ResetPassword = () => {
+  return <div></div>;
+};
+
+export default ResetPassword;
